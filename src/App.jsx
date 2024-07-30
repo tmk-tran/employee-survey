93d675d3e4746448a4c8178d4939f00433b170f2
T Mark <tmk1.tran@gmail.com>
2024-07-30 16:37:00 -0500

    imported project from PixelFree

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import SurveyCard from './components/SurveyCard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Employee Survey - PixelFree
+      </header>
+      <SurveyCard />
+    </div>
+  );
+}
+
+export default App;
